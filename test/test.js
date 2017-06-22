be3f49e089ca8e5d54ab5328e5f3e277202933e5
Olejnik Stanisław <olejniks@gmk.local>
2017-06-22 09:00:19 +0200

    test jednostek - miar

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var assert = require('assert');
+//var assert = require('assert');
+var assert = require('chai').assert;
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
@@ -7,3 +9,15 @@ describe('Array', function() {
   });
 });
 
+var tabJedn=['km','m','dcm','cm','mm'],
+	jednTest ='km' ;
+
+
+describe ('jednostki',function(){
+	describe('miary',function(){
+		it('czy jednostka "'+jednTest+'" jest w tabeli: ['+tabJedn+']',function(){
+			//assert.isTrue(tabJedn.indexOf('km'));
+			assert.isAtLeast(tabJedn.indexOf(jednTest), -1, 'jednostka jest w tabeli');
+		});
+	});
+});
